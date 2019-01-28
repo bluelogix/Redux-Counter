f3ed0813bbdf05d97a0f1b23803618b250d76460
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-01-28 17:30:49 -0500

    mvp complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
+
+
+
+
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
@@ -20,10 +24,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => /* Fill me in */ this.props.increment() }>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => /* Fill me in */ this.props.decrement() }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
